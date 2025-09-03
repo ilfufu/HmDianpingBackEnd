d5278ed3da32b7195e98c46c4c8614939c0498d5
ilfufu <hou02101029@163.com>
2025-09-03 12:47:38 +0800

    second kill based on redis function finish for the first time

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +25,53 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    public Result seckillVoucher(Long voucherId){
+        // 查询优惠券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+
+        // 查询是否开始
+        if(voucher.getBeginTime().isAfter(LocalDateTime.now())){
+            return Result.fail("秒杀未开始");
+        }
+
+        // 查询是否结束
+        if(voucher.getEndTime().isBefore(LocalDateTime.now())){
+            return Result.fail("秒杀已结束");
+        }
+
+        // 尝试扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId)
+                .update();
+
+        // 库存不足扣减失败
+        if(!success){
+            return Result.fail("库存不足");
+        }
+
+        // 创建并保存订单信息
+        VoucherOrder voucherOrder = new VoucherOrder();
+        // 唯一ID
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+
+        // 用户ID
+        Long userId = UserHolder.getUser().getId();
+        voucherOrder.setUserId(userId);
+
+        // 秒杀ID
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+
+        // 返回订单ID
+        return Result.ok(orderId);
+    }
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -4,10 +4,14 @@ import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
@@ -18,6 +22,38 @@ class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWorker(){
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+              for (int i = 0; i < 100; i++) {
+                  long id = redisIdWorker.nextId("order");
+                  System.out.println("id = " + id);
+              }
+              latch.countDown();
+        };
+        Long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+
+        try{
+            latch.await();
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        Long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
 
     @Test
     void testSaveShop() throws InterruptedException {
