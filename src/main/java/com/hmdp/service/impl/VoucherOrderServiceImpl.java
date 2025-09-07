ec3fd64de3ddac06ae374f92a9d58921c2c1bdc2
ilfufu <hou02101029@163.com>
2025-09-07 21:47:29 +0800

    distributed lock based on redis complete for the first time

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -8,8 +8,11 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
 import org.springframework.aop.framework.AopContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +35,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
     @Resource
     private RedisIdWorker redisIdWorker;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public Result seckillVoucher(Long voucherId){
@@ -51,12 +56,30 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         // 改进：一人一单
         Long userId = UserHolder.getUser().getId();
 
+        // 创建锁对象
+        SimpleRedisLock lock = new SimpleRedisLock("order:" + userId, stringRedisTemplate);
+
+        // 尝试获取锁
+        boolean isLock = lock.tryLock(1200);
+
+        // 获取失败
+        if(!isLock){
+            // 返回错误信息
+            return Result.fail("不允许重复下单");
+        }
+
         // 加锁防止一人一单功能失效
-        synchronized (userId.toString().intern()) {
+        // synchronized (userId.toString().intern()) {
+        try {
             // 获取代理对象（事务）
             IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
             return proxy.createVoucherOrder(voucherId, userId);
         }
+        finally {
+            // 释放锁
+            lock.unlock();
+        }
+        // }
     }
 
     @Transactional
